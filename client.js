f4d345a63c942c03bac61b5512b9c0aaed37a0c7
kethnal <kaseyvaldez@hotmail.com>
2022-09-28 13:41:40 -0700

    make input recognize ctrl to exit

--- client.js
+++ client.js
@@ -1,6 +1,6 @@
 const net = require("net");
 
-const connect = function () {
+const connect = function() {
   const conn = net.createConnection({
     host: '165.227.47.243',
     port: 50541
@@ -9,9 +9,6 @@ const connect = function () {
   conn.on("connect", () => {
     console.log("Connection successful!");
     conn.write("Name: KV");
-//    setInterval(() => {
-//      conn.write("Move: up");
-//    }, 50);
   });
 
   conn.on("data", (data) => {
